9e14a899184cba9caa471e3ffb7da90ad7516394
Nii1214 <nii.tomoyuki1214@gmail.com>
2026-01-12 11:58:59 +0900

    feat: ToDo登録- UIコンポーネントを追加

---
+++ src/components/common/Message.tsx
@@ -0,0 +1,17 @@
+'use Client'
+
+type Props = {
+    text: string;
+    type: "success" | "error" | "info";
+};
+
+export default function Message({ text, type }: Props) {
+    let colorClass = "";
+    switch (type) {
+        case "success": colorClass = "text-green-600"; break;
+        case "error": colorClass = "text-red-600"; break;
+        case "info": colorClass = "text-blue-600"; break;
+    }
+
+    return <p className={colorClass}>{text}</p>;
+}

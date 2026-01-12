89f09a1535aa9e738df817588630cd866fc591ec
Nii1214 <nii.tomoyuki1214@gmail.com>
2026-01-12 11:59:54 +0900

    feat: ToDo登録- ページ・フォームの追加

---
+++ src/app/todos/TodoCreateForm.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import { createTodoAction } from "./actions/createTodo";
+import Message from "@/components/common/Message";
+import { useMessage } from "@/hooks/useMessage";
+
+export default function TodoCreateForm() {
+
+    const [title,setTitle] = useState("");
+    const {message, showMessage,clearMessage} = useMessage();
+
+    async function handleCreate(){
+        clearMessage();
+        // trim:文字列の前後にある空白を削除する
+        if(!title.trim()) {
+            showMessage("Todo を入力してください", "info");
+            return;
+        }
+
+        try {
+            await createTodoAction({ title });
+            showMessage("✅ 登録が完了しました", "success");
+            setTitle("");
+        } catch (error: any) {
+            showMessage(`❌ 登録に失敗しました: ${error.message}`, "error");
+        }
+    }
+
+    return (
+        <div className="max-w-md mx-auto">
+            <div className="flex gap-3">
+                <input 
+                    type="text"
+                    name="title"
+                    placeholder="Todoを入力"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
+                <button onClick={handleCreate}>登録</button>
+            </div>
+            {/* 　messageがnullでない時だけ<Message>を表示する */}
+            {message && (<Message text={message.text} type={message.type}/>)}
+        </div>
+    );
+}
---
+++ src/app/todos/page.tsx
@@ -0,0 +1,10 @@
+import TodoCreateForm from "./TodoCreateForm";
+
+export default function TodoPage() {
+    return (
+        <div>
+            <h1>Todo一覧</h1>
+            <TodoCreateForm />
+        </div>
+    )
+}

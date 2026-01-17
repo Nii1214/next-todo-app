5e3c383b59786da2505d51883eae898f5ffbe220
Nii1214 <nii.tomoyuki1214@gmail.com>
2026-01-17 15:11:48 +0900

    fix: use serverを削除

--- src/domain/todo/createTodoUseCase.ts
+++ src/domain/todo/createTodoUseCase.ts
@@ -1,5 +1,3 @@
-'use server';
-
 import { validateTodo } from "@/utils/validateTodo";
 import { TodoRepository } from "@/repositories/todoRepository"
 /**
@@ -9,18 +7,18 @@ import { TodoRepository } from "@/repositories/todoRepository"
  * @param todo 
  */
 export async function createTodoUseCase(
-    todo: { title: string },
-    repository: TodoRepository
-  ) {
-    const { title } = todo
-  
-    const error = validateTodo(title)
-    if (error) {
-      throw new Error(error)
-    }
-  
-    await repository.create({
-      title,
-      is_done: false,
-    })
+  todo: { title: string },
+  repository: TodoRepository
+) {
+  const { title } = todo
+
+  const error = validateTodo(title)
+  if (error) {
+    throw new Error(error)
+  }
+
+  await repository.create({
+    title,
+    is_done: false,
+  })
 }
